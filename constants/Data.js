b923213cc8e1daa6760380a9ad56221a8b505cf3
qudus <coodous@gmail.com>
2021-11-17 21:05:00 +0000

    added new images

--- constants/Data.js
+++ constants/Data.js
@@ -4,7 +4,7 @@ export const DATA = [
     {
         id: 0,
         name: "Nike Metcon 4",
-        img: images.nikeMetcon4,
+        img: images.shirt1,
         bgColor: "#414045",
         type: "TRAINING",
         price: "$119",
@@ -13,7 +13,7 @@ export const DATA = [
     {
         id: 1,
         name: "Nike Metcon 6",
-        img: images.nikeMetcon6,
+        img: images.shirt2,
         bgColor: "#4EABA6",
         type: "TRAINING",
         price: "$135",
@@ -22,7 +22,7 @@ export const DATA = [
     {
         id: 2,
         name: "Nike Metcon 5",
-        img: images.nikeMetcon5Purple,
+        img: images.shirt3,
         bgColor: "#2B4660",
         type: "TRAINING",
         price: "$124",
@@ -31,7 +31,7 @@ export const DATA = [
     {
         id: 3,
         name: "Nike Metcon 3",
-        img: images.nikeMetcon3,
+        img: images.shirt4,
         bgColor: "#A69285",
         type: "TRAINING",
         price: "$99",
@@ -40,7 +40,7 @@ export const DATA = [
     {
         id: 4,
         name: "Nike Metcon Free",
-        img: images.nikeMetconFree,
+        img: images.shirt5,
         bgColor: "#A02E41",
         type: "TRAINING",
         price: "$108",
@@ -49,28 +49,10 @@ export const DATA = [
     {
         id: 5,
         name: "Nike Air Zoom Pegasus 36",
-        img: images.nikePegasus36,
+        img: images.shirt6,
         bgColor: "#BF012C",
         type: "RUNNING",
         price: "$186",
         sizes: [6, 7, 8, 9, 10]
-    },
-    {
-        id: 6,
-        name: "Nike Metcon 5",
-        img: images.nikeMetcon5Black,
-        bgColor: "#D39C67",
-        type: "TRAINING",
-        price: "$135",
-        sizes: [6, 7, 8, 9, 10, 11, 12]
-    },
-    {
-        id: 7,
-        name: "Nike Air Zoom Kobe 1 Proto",
-        img: images.nikeZoomKobe1Proto,
-        bgColor: "#7052A0",
-        type: "BASKETBALL",
-        price: "$199",
-        sizes: [6, 7, 8, 9]
     },
 ]
